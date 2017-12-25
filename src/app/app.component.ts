dd0a9d53fdc521dce1154a141f087240b5e749fe
Sambit Kumar Padhy <prsambit6@gmail.com>
2017-12-25 22:04:18 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+	tasks : string[] = [];
+
+	add(task : string) {
+		if(task) {
+			this.tasks.push(task);
+		}
+	}
+
+	remove(task : string) {
+		if(task) {
+			const index: number = this.tasks.indexOf(task);
+			if (index !== -1) {
+				this.tasks.splice(index, 1);
+			}    
+		}
+	}
 }
